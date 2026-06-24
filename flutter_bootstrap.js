b1eeb12a9c8641cc702278e5858d803bf88ec2c6
LuizErler <LuizErler@users.noreply.github.com>
2026-06-24 02:07:41 +0000

    deploy: bf5528e94e9dc65dfb00ae90b57795d0d26dd247

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3396997875" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2381476398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
